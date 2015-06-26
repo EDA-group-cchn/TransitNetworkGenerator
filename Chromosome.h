b64845d01416900b7646ac5512248f421523ee9e
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-06-25 19:37:26 -0500

    chromosome size

--- Chromosome.h
+++ Chromosome.h
@@ -13,7 +13,10 @@ private:
   std::vector<Gene> genes;
 public:
   Chromosome() { };
-  const Gene &getGene(int id) const {
+  size_t size() {
+    return genes.size();
+  }
+  const Gene &getGene(size_t id) const {
     return genes[id];
   }
   void addGene(const Gene &gene) {
