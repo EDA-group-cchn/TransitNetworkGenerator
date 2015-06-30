aafa7178076b335f569f532745ecd2658594eeb0
Jose Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-06-30 08:53:04 -0500

    Generate Solution Graph #4

--- Chromosome.cpp
+++ Chromosome.cpp
@@ -1,8 +1,28 @@
 #include "Chromosome.h"
 
 Graph Chromosome::generateSolutionGraph(const Graph &original,
-                                        RoutesCache &routesCache) const {
-  return Graph();
+                                         RoutesCache &routesCache) const {
+  
+  Graph new_graph(original.getVertexCount());
+  for (Gene g : genes)
+  {
+    Route r;
+    if (routesCache.check(g)){
+        r = routesCache.get(g);
+    }
+    else{
+        r = g.calculateBestRoute(original);
+    }
+    int from = r.getFirstVertex();
+    std::vector<int> edges = r.getEdgeList();
+    for(int e : edges)
+    {
+        new_graph.addEdge(from, original.getAdjacentVertex(e),
+                          original.getWeight(e));
+    }
+
+  }
+  return new_graph;
 }
 
 float Chromosome::calculateCost(const Graph &original,
