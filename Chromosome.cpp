17bdab77ff40c0b46b10bdab432308f8f83a6626
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-06-30 09:48:14 -0500

    calculateCost

--- Chromosome.cpp
+++ Chromosome.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "Chromosome.h"
 
 Graph Chromosome::generateSolutionGraph(const Graph &original,
@@ -18,7 +19,6 @@ Graph Chromosome::generateSolutionGraph(const Graph &original,
       new_graph.addEdge(from, original.getAdjacentVertex(e),
                         original.getWeight(e));
     }
-
   }
   return new_graph;
 }
@@ -26,5 +26,21 @@ Graph Chromosome::generateSolutionGraph(const Graph &original,
 float Chromosome::calculateCost(const Graph &original,
                                 const std::vector<Passenger> &passengers,
                                 RoutesCache &routesCache) const {
-  return 0;
+  Graph solutionGraph = generateSolutionGraph(original, routesCache);
+  Dijkstra dijkstra(solutionGraph);
+  for (int i = 0; i < original.getVertexCount(); ++i)
+    dijkstra.makeDijkstra(i);
+  float meanCost = 0;
+  for (int i = 0; i < passengers.size(); ++i) {
+    const Passenger &passenger = passengers[i];
+    float cost = dijkstra.getWeight(passenger.getBusStopStartId(),
+                                        passenger.getBusStopEndId());
+    if (cost < std::numeric_limits<float>::max())
+      cost += passenger.getBusStopStartDistance() +
+          passenger.getBusStopEndDistance();
+    else
+      cost += meanCost + 100; // a penalty for not serving this passenger
+    meanCost = (meanCost * i + cost) / (i + 1);
+  }
+  return meanCost;
 }
