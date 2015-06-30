61ec32721765e0e9a4ee9c23eae46b7419c96b57
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-06-30 09:48:01 -0500

    indentation & marking nodes as visited

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,64 +1,61 @@
 #include "Dijkstra.h"
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <limits>
 
-#define INF 999999
 
 struct comparator{
-    bool operator() ( std::pair<int, float> i, std::pair <int, float> j ){
-        return i.second > j.second;
-        }
+  bool operator() ( std::pair<int, float> i, std::pair <int, float> j ){
+    return i.second > j.second;
+  }
 };
 
 void Dijkstra::makeDijkstra(index from)
 {
-    std::priority_queue< std::pair<int, float>, 
-                        std::vector<std::pair<int, float> >,
-                        comparator > route_queue;
+  std::priority_queue< std::pair<int, float>,
+      std::vector<std::pair<int, float>>,
+      comparator > route_queue;
 
-    for (int i = 0; i < graph.getVertexCount(); ++i)
-    {
-        dist[from][i] = std::numeric_limits<float>::max();;
-        father[from][i] = std::make_pair(-1,-1);
+  std::vector<bool> seen((size_t)graph.getVertexCount());
+
+  for (int i = 0; i < graph.getVertexCount(); ++i)
+  {
+    dist[from][i] = std::numeric_limits<float>::max();;
+    father[from][i] = std::make_pair(-1,-1);
+  }
+
+  dist[from][from]  = 0;
+  route_queue.push(std::make_pair(from, 0));
+
+  while (route_queue.size() != 0) {
+    int u  = route_queue.top().first;
+    route_queue.pop();
+    if (seen[u])
+      continue;
+    seen[u] = true;
+
+    for (int e : graph.getIncidentEdges(u)) {
+
+      int v = graph.getAdjacentVertex(e);
+      if (dist[from][v] > (dist[from][u] + graph.getWeight(e))){
+
+        dist[from][v] = dist[from][u] + graph.getWeight(e);
+        father[from][v] = std::make_pair(u, e);
+
+        route_queue.push(std::make_pair(v, dist[from][v]));
+      }
     }
-
-    dist[from][from]  = 0;
-    route_queue.push(std::make_pair(from, 0));
-    int last = INF;
-
-    while (route_queue.size() != 0){
-    
-        int u  = route_queue.top().first;
-    
-        for (int e: graph.getIncidentEdges(u)){
-            
-            int v = graph.getAdjacentVertex(e);
-            if (dist[from][v] > (dist[from][u] + graph.getWeight(e))){
-                
-                dist[from][v] = dist[from][u] + graph.getWeight(e);
-                father[from][v] = std::make_pair(u, e);
-
-                route_queue.push(std::make_pair(v, dist[from][v]));
-            }
-        }
-
-        route_queue.pop();
-    }
+  }
 }
 
 Route Dijkstra::getPath(index from, index to)
 {
-    index temp = to;
-    std::vector<int> edges;
-    while(temp != from)
-    {
-        edges.push_back(father[from][temp].second);
-        temp = father[from][temp].first;
-    }
+  index temp = to;
+  std::vector<int> edges;
+  while(temp != from)
+  {
+    edges.push_back(father[from][temp].second);
+    temp = father[from][temp].first;
+  }
 
-    std::reverse(edges.begin(), edges.end());
+  std::reverse(edges.begin(), edges.end());
 
-    return Route(from, edges);
+  return Route(from, edges);
 }
--- Dijkstra.h
+++ Dijkstra.h
@@ -4,52 +4,47 @@
 #include "Graph.h"
 #include "Route.h"
 #include <vector>
+#include <queue>
+#include <algorithm>
+#include <limits>
+
 
 class Dijkstra
 {
 public:
-    typedef float weight;
-    typedef int index;
-    typedef int edge;
-    Dijkstra(const Graph & graph)  :  graph(graph){
-        int n = graph.getVertexCount();
-        dist.resize(n);
-        father.resize(n);
-        for (int i = 0; i < n; ++i)
-        {
-            father[i].resize(n);
-            dist[i].resize(n);
-        }
-    }
-    Dijkstra(const Graph & graph, index from) : graph(graph)
+  typedef float weight;
+  typedef int index;
+  typedef int edge;
+  Dijkstra(const Graph & graph)  :  graph(graph) {
+    int n = graph.getVertexCount();
+    dist.resize((std::size_t)n);
+    father.resize((std::size_t)n);
+    for (int i = 0; i < n; ++i)
     {
-        int n = graph.getVertexCount();
-        dist.resize(n);
-        father.resize(n);
-        for (int i = 0; i < n; ++i)
-        {
-            father[i].resize(n);
-            dist[i].resize(n);
-        }
-        makeDijkstra(from);
-
-    }  
-    ~Dijkstra(){}
-
-
-    void makeDijkstra(index from);
-
-    weight getWeight(index from, index to){
-        return dist[from][to];
+      father[i].resize((std::size_t)n);
+      dist[i].resize((std::size_t)n);
     }
+  }
+  Dijkstra(const Graph & graph, index from) : Dijkstra(graph)
+  {
+    makeDijkstra(from);
+  }
+  ~Dijkstra(){}
 
-    Route getPath(index from, index to);
+
+  void makeDijkstra(index from);
+
+  weight getWeight(index from, index to){
+    return dist[from][to];
+  }
+
+  Route getPath(index from, index to);
 
 
 private:
-    std::vector<std::vector<weight> > dist;
-    std::vector<std::vector< std::pair <index, edge > > > father;
-    const Graph &graph;
+  std::vector<std::vector<weight> > dist;
+  std::vector<std::vector< std::pair <index, edge > > > father;
+  const Graph &graph;
 
 };
 
