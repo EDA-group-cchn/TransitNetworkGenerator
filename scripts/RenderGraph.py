247adf01feb218f7d082ee4533cb4edfca5db31f
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-07-04 02:03:12 -0500

    adding colors

--- scripts/RenderGraph.py
+++ scripts/RenderGraph.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 from graphviz import Digraph
 from sys import argv
 
@@ -14,9 +16,12 @@ def main():
     filename = argv[1]
     graph_data = getGraphData(filename)
     graph = Digraph()
-    for route in graph_data:
+    colors = ['red', 'blue', 'green', 'black', 'blueviolet', 'aquamarine',
+              'orange', 'yellow4', 'indigo', 'turquoise', 'violetred',
+              'steelblue']
+    for route, color in zip(graph_data, colors[:len(graph_data)]):
         for i in range(len(route)-1):
-            graph.edge(route[i], route[i+1], color="red")
+            graph.edge(route[i], route[i+1], color=color, penwidth="1.5")
     graph.format = 'png' #change format of output
     graph.render()
 
