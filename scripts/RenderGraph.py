2718348056e345955f0ae408fee3715868e75d8a
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-07-04 00:27:13 -0500

    reducing code

--- scripts/RenderGraph.py
+++ scripts/RenderGraph.py
@@ -5,10 +5,8 @@ from sys import argv
 def getGraphData(filename):
     routes = []
     with open(filename, 'r') as file:
-        content = file.readlines()
-        for x in content:
+        for x in file:
             routes.append(x.strip().split(' '))
-        file.close()
     return routes
 
 
@@ -17,11 +15,9 @@ def main():
     graph_data = getGraphData(filename)
     graph = Digraph()
     for route in graph_data:
-        for vertex in route:
-            graph.node(vertex)
         for i in range(len(route)-1):
             graph.edge(route[i], route[i+1], color="red")
-#    graph.format = 'png' change format of output
+    graph.format = 'png' #change format of output
     graph.render()
 
 
