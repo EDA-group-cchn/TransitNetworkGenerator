f15bab2d7368c35d8d44735f0dddf012018780da
Aldo Culquicondor <aldo.culquicondor@ucsp.edu.pe>
2015-07-04 10:22:40 -0500

    bugfix in originalRoute regeneration

--- SubGraph.cpp
+++ SubGraph.cpp
@@ -7,7 +7,8 @@ Route SubGraph::getOriginalRoute(const std::vector<int> &r)
   int from = getOriginalVertexId(r[0]);
 
   for (size_t i = 1; i < r.size(); ++i) {
-    Route tmp_route = dijkstra->getPath(r[i-1], r[i]);
+    Route tmp_route = dijkstra->getPath(getOriginalVertexId(r[i-1]),
+                                        getOriginalVertexId(r[i]));
     edges_result.insert(edges_result.end(),
                         tmp_route.getEdgeList().begin(),
                         tmp_route.getEdgeList().end()) ;
